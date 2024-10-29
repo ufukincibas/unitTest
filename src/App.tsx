11b8b333d4abc265d11a51570e00ec249eeb34ff
ufukincibas <ufukincibas@hotmail.com>
2024-10-29 21:58:21 +0300

    basic to_do app test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
-import React from "react";
-import { Text , View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text , TextInput, View } from "react-native";
 import Button from "./components/Button";
 
 function App(){
+
+  const [text , setText] = useState("")
+  const [list , setList] = useState([])
+
+const renderElements = ({item}) => <Text>{item}</Text>
+
+function addToList() {
+if(!text) {
+  return;
+}
+  setList([...list , text])
+}
+
   return(
-    <View>
+    <View style = {styles.container}>
       <Text>Hello Testing</Text>
-     <Button title="Testing is Easy"/>
+      <FlatList
+      testID="list"
+      keyExtractor={(_ , i) => i.toString()} 
+      data={list} 
+      renderItem={renderElements}/>
+
+      <TextInput 
+      testID="input-area"
+      placeholder = "add... "
+      onChangeText={setText}
+      style={styles.input}/>
+     <Button title="Add.." onClick={addToList}/>
     </View>
   )
 }
 
 export default App;
+
+
+const styles = StyleSheet.create({
+  container : { flex:1},
+  input : {
+    backgroundColor: "#e0e0e0" ,
+    margin : 10 ,
+    padding : 10 ,
+    borderRadius:10,
+
+  } ,
+})
--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -2,16 +2,20 @@ import { render , fireEvent } from "@testing-library/react-native"
 import Button from "./Button"
 import styles from "./Button.styles"
 
-
+//snapshot testing
 test("should match with snapshot" ,  () => {
     const comp = render(<Button />) //icine sorgu gonderecegiz test edilebilir yapıyı saglar
     expect(comp).toMatchSnapshot();//snapshot a uyuşup uyuşmadığını kontrol eder
 });
 
+
+//basic expect().toBe() structure
 test("check number" , () => {
     expect(5+6).toBe(11)  //5+6 nın 11 olmasını bekliyorum gibi bir kullanımı  var
 });
 
+
+ //Title Testing
 test("should render title correctly" , () => {
     const testTitle = "test"
     const comp = render(<Button title={testTitle}/>) 
@@ -20,6 +24,8 @@ test("should render title correctly" , () => {
 
     expect(buttonText).toBe(testTitle) //title gonderince render oluyor mu kontrolu
 })
+
+//onPress Testing
 
 test("should triger onPress correctly" , () => {
     const onClickFunction = jest.fn(); //jestin sagladıgı ozel bir func tetiklenip tetiklenmedigini kontrol etmek icin
@@ -32,7 +38,7 @@ test("should triger onPress correctly" , () => {
     expect(onClickFunction).toBeCalled();
 })
 
-
+ //Style Testing
 test("should render default given theme style" , () => {
     
     const comp = render(<Button />)
@@ -43,7 +49,7 @@ test("should render default given theme style" , () => {
    
 } )
 
-
+    //Style Testing
 test("should render given theme style" , () => {
     const selectedTheme = "SECONDARY"
     const comp = render(<Button theme={selectedTheme}/>)
